e632c4a56328dc190e99c5bba99974ff3b1d3421
Volkau Aliaksey <stakebf@gmail.com>
2020-09-29 13:01:25 +0300

    feat: complete count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  // return matrix.flat().filter(item => item === '^^').length;
+  const cats = [];
+  // let counterCats = 0;
+  matrix.forEach(arrItems => arrItems.forEach(item => {
+    item === '^^' ? cats.push(item) : item;
+  }));
+  // for (let i = 0; i < matrix.length; i++) {
+  // for (let j = 0; j < matrix[i].length; j++) {
+  // matrix[i][j] === '^^' ? cats.push(matrix[i][j]) : matrix[i][j];
+  // if (matrix[i][j] === '^^') {
+  // counterCats++;
+  // }
+  // }
+  // }
+  return cats.length;
+  // return counterCats;
 };
