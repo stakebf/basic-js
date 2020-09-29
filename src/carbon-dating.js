5636385cb174a5e4c84d367437abc1d43ea534f5
Volkau Aliaksey <stakebf@gmail.com>
2020-09-29 13:31:22 +0300

    feat: complete carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,22 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (
+    typeof sampleActivity !== 'string' ||
+    !sampleActivity ||
+    isNaN(parseFloat(sampleActivity))
+  ) {
+    return false;
+  }
+
+  if (sampleActivity < 1 || sampleActivity > 15) {
+    return false;
+  }
+
+  return Math.ceil(
+    Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD)
+  );
 };
